79e454d95b4bd5f90800a52cc9f7bb2444aad65b
Patrick van Elderen <patrick.vanelderen@live.nl>
2021-12-19 13:58:02 +0100

    no message

--- Valinor-S/src/com/valinor/game/world/entity/combat/method/impl/npcs/bosses/Skotizo.java
+++ Valinor-S/src/com/valinor/game/world/entity/combat/method/impl/npcs/bosses/Skotizo.java
@@ -21,93 +21,30 @@ public class Skotizo extends CommonCombatMethod {
 
     @Override
     public void prepareAttack(Mob mob, Mob target) {
-        int roll = new SecureRandom().nextInt(5);
-
-        //10% chance that the wold boss skulls you!
-        if (World.getWorld().rollDie(10, 1)) {
-            Skulling.assignSkullState(((Player) target), SkullType.WHITE_SKULL);
-            target.message("The "+mob.getMobName()+" has skulled you, be careful!");
-        }
-
-        Npc npc = (Npc) mob;
-        if (roll == 1) {
-            //System.out.println("rolled one, using ranged attack.");
-            ranged_attack(npc, target);
-        } else if (roll == 2) {
-            //System.out.println("rolled two, using magic attack.");
-            magic_attack(npc, target);
-        } else {
-            if (CombatFactory.canReach(npc, CombatFactory.MELEE_COMBAT, target)) {
-               // System.out.println("melee distance, using melee attack.");
-                melee_attack(npc, target);
-            } else if (Utils.rollDie(2, 1)) {
-                magic_attack(npc, target);
-               // System.out.println("Otherwise rolled magic attack due to out of melee distance.");
-            } else {
-                ranged_attack(npc, target);
-               // System.out.println("Otherwise rolled ranged attack due to out of melee distance.");
-            }
-        }
+        if (withinDistance(1) && World.getWorld().rollDie(3, 2))
+            meleeAttack(mob, target);
+        else
+            magicAttack(mob, target);
     }
 
     /**
      * Handles the melee attack
      */
-    private void melee_attack(Npc npc, Mob target) {
-        Tile tile = target.tile();
-
-        World.getWorld().getPlayers().forEach(player -> {
-            if (tile.inSqRadius(player.tile(), 2)) {
-                //Wild boss, should deal damage regardless of prayer and don't check attack accuracy makes it harder!
-                target.hit(npc, CombatFactory.calcDamageFromType(npc, player, CombatType.MELEE), CombatType.MELEE).checkAccuracy().submit();
-            }
-        });
-
-        npc.animate(npc.attackAnimation());
-        npc.getTimers().register(TimerKey.COMBAT_ATTACK, npc.combatInfo().attackspeed);
+    private void meleeAttack(Mob mob, Mob target) {
+        mob.animate(mob.attackAnimation());
+        target.hit(mob, CombatFactory.calcDamageFromType(mob, target, CombatType.MELEE), 0, CombatType.MELEE).checkAccuracy().submit();
     }
 
     /**
      * Handles the magic attack
      */
-    private void magic_attack(Npc npc, Mob target) {
-        Tile tile = target.tile();
+    private void magicAttack(Mob mob, Mob target) {
+        mob.animate(69);
+        Projectile projectile = new Projectile(mob, target, 1242, 25, mob.projectileSpeed(target), 120, 31,0);
+        projectile.sendProjectile();
 
-        World.getWorld().getPlayers().forEach(player -> {
-            if (tile.inSqRadius(player.tile(), 3)) {
-                int tileDist = npc.tile().transform(3, 3, 0).distance(player.tile());
-                int delay = Math.max(1, (30 + (tileDist * 12)) / 30);
-
-                new Projectile(npc, player, 165, 20, 12 * tileDist, 80, 30, 0).sendProjectile();
-
-                target.hit(npc, CombatFactory.calcDamageFromType(npc, player, CombatType.MAGIC), delay, CombatType.MAGIC).checkAccuracy().submit();
-                player.delayedGraphics(new Graphic(166, 90, 0), delay);
-            }
-        });
-        npc.animate(69);
-        npc.getTimers().register(TimerKey.COMBAT_ATTACK, npc.combatInfo().attackspeed);
-    }
-
-
-    /**
-     * Handles the ranged attack
-     */
-    private void ranged_attack(Npc npc, Mob target) {
-        Tile tile = target.tile();
-
-        World.getWorld().getPlayers().forEach(player -> {
-            if (tile.inSqRadius(player.tile(), 3)) {
-                int tileDist = npc.tile().transform(3, 3, 0).distance(player.tile());
-                int delay = Math.max(1, (30 + (tileDist * 12)) / 30);
-
-                new Projectile(npc, player, 1242, 20, 12 * tileDist, 80, 50, 0).sendProjectile();
-                target.hit(npc, CombatFactory.calcDamageFromType(npc, player, CombatType.RANGED), delay, CombatType.RANGED).checkAccuracy().submit();
-                player.delayedGraphics(new Graphic(1243, 80, 0), delay);
-            }
-        });
-
-        npc.animate(69);
-        npc.getTimers().register(TimerKey.COMBAT_ATTACK, npc.combatInfo().attackspeed);
+        target.hit(mob, CombatFactory.calcDamageFromType(mob, target, CombatType.MAGIC), mob.getProjectileHitDelay(target), CombatType.MAGIC).checkAccuracy().submit();
+        target.delayedGraphics(new Graphic(197,0,0), mob.getProjectileHitDelay(target));
     }
 
     @Override
@@ -117,6 +54,6 @@ public class Skotizo extends CommonCombatMethod {
 
     @Override
     public int getAttackDistance(Mob mob) {
-        return 7;
+        return 8;
     }
 }
