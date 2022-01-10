ad6eea70a0f7f8ced59f30043521c387da1f6b53
Patrick van Elderen <patrick.vanelderen@live.nl>
2022-01-10 14:31:41 +0100

    - The healing pool at home now heals spec for members.

--- Valinor-S/src/com/valinor/game/content/areas/home/HomeArea.java
+++ Valinor-S/src/com/valinor/game/content/areas/home/HomeArea.java
@@ -43,13 +43,13 @@ public class HomeArea extends Interaction {
 
     @Override
     public boolean handleObjectInteraction(Player player, GameObject object, int option) {
-        if(option == 1) {
-            if(object.getId() == CLOSED_CHEST_2996) {
+        if (option == 1) {
+            if (object.getId() == CLOSED_CHEST_2996) {
                 player.faceObj(object);
                 player.getSlayerKey().open();
                 return true;
             }
-            if(object.getId() == PORTAL_15477) {
+            if (object.getId() == PORTAL_15477) {
                 player.faceObj(object);
                 player.message(Color.RED.wrap("Construction is being worked on and not ready to be released yet."));
                 return true;
@@ -77,31 +77,26 @@ public class HomeArea extends Interaction {
                     Poison.cure(player);
                     Venom.cure(2, player);
 
-                    if (player.tile().region() != 13386) {
+                    if (!player.getMemberRights().isSapphireMemberOrGreater(player))
                         player.message(Color.RED.tag() + "When being a member your special attack will also regenerate.");
-                        if (player.getMemberRights().isSapphireMemberOrGreater(player)) {
-                            if (player.getTimers().has(TimerKey.RECHARGE_SPECIAL_ATTACK)) {
-                                player.message("Special attack energy can be restored in " + player.getTimers().asMinutesAndSecondsLeft(TimerKey.RECHARGE_SPECIAL_ATTACK) + ".");
-                            } else {
-                                player.restoreSpecialAttack(100);
-                                player.setSpecialActivated(false);
-                                CombatSpecial.updateBar(player);
-                                int time = 0;
-                                if (player.getMemberRights().isSapphireMemberOrGreater(player))
-                                    time = 300;//3 minutes
-                                if (player.getMemberRights().isEmeraldMemberOrGreater(player))
-                                    time = 100;//1 minute
-                                if (player.getMemberRights().isRubyMemberOrGreater(player))
-                                    time = 0;//always
-                                player.getTimers().register(TimerKey.RECHARGE_SPECIAL_ATTACK, time); //Set the value of the timer.
-                                player.message("<col=" + Color.HOTPINK.getColorValue() + ">You have restored your special attack.");
-                            }
+
+                    if (player.getMemberRights().isSapphireMemberOrGreater(player)) {
+                        if (player.getTimers().has(TimerKey.RECHARGE_SPECIAL_ATTACK)) {
+                            player.message("Special attack energy can be restored in " + player.getTimers().asMinutesAndSecondsLeft(TimerKey.RECHARGE_SPECIAL_ATTACK) + ".");
+                        } else {
+                            player.restoreSpecialAttack(100);
+                            player.setSpecialActivated(false);
+                            CombatSpecial.updateBar(player);
+                            int time = 0;
+                            if (player.getMemberRights().isSapphireMemberOrGreater(player))
+                                time = 300;//3 minutes
+                            if (player.getMemberRights().isEmeraldMemberOrGreater(player))
+                                time = 100;//1 minute
+                            if (player.getMemberRights().isRubyMemberOrGreater(player))
+                                time = 0;//always
+                            player.getTimers().register(TimerKey.RECHARGE_SPECIAL_ATTACK, time); //Set the value of the timer.
+                            player.message("<col=" + Color.HOTPINK.getColorValue() + ">You have restored your special attack.");
                         }
-                    } else {
-                        player.restoreSpecialAttack(100);
-                        player.setSpecialActivated(false);
-                        CombatSpecial.updateBar(player);
-                        player.message("<col=" + Color.HOTPINK.getColorValue() + ">You have restored your special attack.");
                     }
                     player.unlock();
                 });
@@ -114,7 +109,7 @@ public class HomeArea extends Interaction {
 
             if (object.getId() == ANTIDRAGON_SHIELD) {
                 Item item = new Item(ItemIdentifiers.ANTIDRAGON_SHIELD);
-                if(player.inventory().hasCapacityFor(item)) {
+                if (player.inventory().hasCapacityFor(item)) {
                     player.animate(536);
                     player.inventory().add(item);
                 } else {
@@ -125,7 +120,7 @@ public class HomeArea extends Interaction {
 
             if (object.getId() == BRONZE_PICKAXE) {
                 Item item = new Item(ItemIdentifiers.BRONZE_PICKAXE);
-                if(player.inventory().hasCapacityFor(item)) {
+                if (player.inventory().hasCapacityFor(item)) {
                     player.animate(536);
                     player.inventory().add(item);
                 } else {
@@ -136,7 +131,7 @@ public class HomeArea extends Interaction {
 
             if (object.getId() == BRONZE_AXE) {
                 Item item = new Item(ItemIdentifiers.BRONZE_AXE);
-                if(player.inventory().hasCapacityFor(item)) {
+                if (player.inventory().hasCapacityFor(item)) {
                     player.animate(536);
                     player.inventory().add(item);
                 } else {
@@ -148,7 +143,7 @@ public class HomeArea extends Interaction {
             if (object.getId() == MOUNTED_MUSIC_CAPE) {
                 Item cape = new Item(ItemIdentifiers.MUSIC_CAPE);
                 Item hood = new Item(ItemIdentifiers.MUSIC_HOOD);
-                if(player.inventory().hasCapacityFor(cape, hood)) {
+                if (player.inventory().hasCapacityFor(cape, hood)) {
                     player.animate(536);
                     player.inventory().addAll(cape, hood);
                 } else {
@@ -164,13 +159,13 @@ public class HomeArea extends Interaction {
                         var canAfford = false;
                         int currencyInInventory = player.inventory().count(COINS_995);
                         if (currencyInInventory > 0) {
-                            if(currencyInInventory >= 50_000_000) {
+                            if (currencyInInventory >= 50_000_000) {
                                 canAfford = true;
-                                player.inventory().remove(new Item(COINS_995, 50_000_000),true);
+                                player.inventory().remove(new Item(COINS_995, 50_000_000), true);
                             }
                         }
 
-                        if(canAfford) {
+                        if (canAfford) {
                             player.animate(536);
                             player.inventory().addAll(cape, hood);
                         } else {
@@ -185,7 +180,7 @@ public class HomeArea extends Interaction {
             if (object.getId() == MOUNTED_QUEST_CAPE_29179) {
                 Item cape = new Item(ItemIdentifiers.QUEST_POINT_CAPE);
                 Item hood = new Item(ItemIdentifiers.QUEST_POINT_HOOD);
-                if(player.inventory().hasCapacityFor(cape, hood)) {
+                if (player.inventory().hasCapacityFor(cape, hood)) {
                     player.animate(536);
                     player.inventory().addAll(cape, hood);
                 } else {
@@ -197,7 +192,7 @@ public class HomeArea extends Interaction {
                 if (AchievementsManager.isCompleted(player, Achievements.COMPLETIONIST)) {
                     Item cape = new Item(ItemIdentifiers.ACHIEVEMENT_DIARY_CAPE);
                     Item hood = new Item(ItemIdentifiers.ACHIEVEMENT_DIARY_HOOD);
-                    if(player.inventory().hasCapacityFor(cape, hood)) {
+                    if (player.inventory().hasCapacityFor(cape, hood)) {
                         player.animate(536);
                         player.inventory().addAll(cape, hood);
                     } else {
@@ -209,7 +204,7 @@ public class HomeArea extends Interaction {
                 return true;
             }
         }
-        if(option == 2) {
+        if (option == 2) {
             if (object.getId() == ANTIDRAGON_SHIELD) {
                 Item item = new Item(ItemIdentifiers.ANTIDRAGON_SHIELD);
                 EquipmentInfo info = World.getWorld().equipmentInfo();
@@ -225,7 +220,7 @@ public class HomeArea extends Interaction {
                 return true;
             }
         }
-        if(option == 3) {
+        if (option == 3) {
 
         }
         return false;
@@ -233,40 +228,40 @@ public class HomeArea extends Interaction {
 
     @Override
     public boolean handleNpcInteraction(Player player, Npc npc, int option) {
-        if(option == 1) {
+        if (option == 1) {
             if (npc.id() == GRAND_EXCHANGE_CLERK || npc.id() == GRAND_EXCHANGE_CLERK_2149) {
                 TradingPost.open(player);
                 return true;
             }
-            if(npc.id() == IRON_MAN_TUTOR) {
+            if (npc.id() == IRON_MAN_TUTOR) {
                 GroupIronmanInterface.open(player);
                 return true;
             }
-            if(npc.id() == PERDU) {
+            if (npc.id() == PERDU) {
                 player.getDialogueManager().start(new PerduDialogue());
                 return true;
             }
             if (npc.id() == SHURA) {
-                if(Referrals.INSTANCE.getCOMMAND_ENABLED()) {
+                if (Referrals.INSTANCE.getCOMMAND_ENABLED()) {
                     player.getDialogueManager().start(new ReferralD());
                 } else {
                     player.message("Referrals are currently disabled.");
                 }
                 return true;
             }
-            if(npc.id() == COOK) {
+            if (npc.id() == COOK) {
                 World.getWorld().shop(13).open(player);
                 return true;
             }
-            if(npc.id() == DOMMIK) {
+            if (npc.id() == DOMMIK) {
                 World.getWorld().shop(12).open(player);
                 return true;
             }
-            if(npc.id() == LOWE) {
+            if (npc.id() == LOWE) {
                 World.getWorld().shop(10).open(player);
                 return true;
             }
-            if(npc.id() == SHOP_ASSISTANT_2820) {
+            if (npc.id() == SHOP_ASSISTANT_2820) {
                 World.getWorld().shop(1).open(player);
                 return true;
             }
@@ -283,17 +278,17 @@ public class HomeArea extends Interaction {
                 return true;
             }
         }
-        if(option == 2) {
-            if(npc.id() == DOMMIK) {
+        if (option == 2) {
+            if (npc.id() == DOMMIK) {
                 World.getWorld().shop(12).open(player);
                 return true;
             }
-            if(npc.id() == LOWE) {
+            if (npc.id() == LOWE) {
                 World.getWorld().shop(10).open(player);
                 return true;
             }
             if (npc.id() == AUBURY) {
-                if(player.gameMode() == GameMode.NONE) {
+                if (player.gameMode() == GameMode.NONE) {
                     World.getWorld().shop(11).open(player);
                 } else {
                     World.getWorld().shop(23).open(player);
@@ -309,7 +304,7 @@ public class HomeArea extends Interaction {
                 return true;
             }
         }
-        if(option == 3) {
+        if (option == 3) {
             if (npc.id() == AUBURY) {
                 npc.forceChat("Seventhior Distine Molenko!");
                 player.graphic(110, 124, 100);
@@ -321,7 +316,7 @@ public class HomeArea extends Interaction {
                 return true;
             }
         }
-        if(option == 4) {
+        if (option == 4) {
 
         }
         return false;
