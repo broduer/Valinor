abd883d85cb62b48eb1b87ff64cc5269fda0787a
Patrick van Elderen <patrick.vanelderen@live.nl>
2022-02-01 14:27:35 +0100

    - Removed all altars that have a talisman from the runecrafting dialogue.

--- Valinor-S/src/com/valinor/game/content/teleport/skill_teles/RunecraftingTeleportsD.java
+++ Valinor-S/src/com/valinor/game/content/teleport/skill_teles/RunecraftingTeleportsD.java
@@ -5,7 +5,6 @@ import com.valinor.game.content.teleport.Teleports;
 import com.valinor.game.world.entity.dialogue.Dialogue;
 import com.valinor.game.world.entity.dialogue.DialogueType;
 import com.valinor.game.world.position.Tile;
-import com.valinor.util.Color;
 import com.valinor.util.timers.TimerKey;
 
 /**
@@ -22,11 +21,6 @@ public class RunecraftingTeleportsD extends Dialogue {
 
     @Override
     protected void select(int option) {
-        if(player.getTimers().has(TimerKey.CLICK_DELAY)) {
-            return;
-        }
-
-        player.getTimers().addOrSet(TimerKey.CLICK_DELAY, 2);
        if(isPhase(0)) {
            if(option == 1) {
                stop();
@@ -35,88 +29,13 @@ public class RunecraftingTeleportsD extends Dialogue {
                }
            }
            if(option == 2) {
-               send(DialogueType.OPTION, DEFAULT_OPTION_TITLE, "Air altar", "Mind altar", "Water altar", "Earth altar", "More...");
+               send(DialogueType.OPTION, DEFAULT_OPTION_TITLE, "Blood altar", "Soul altar", "Nevermind.");
                setPhase(1);
            }
            if(option == 3) {
                stop();
            }
        } else if(isPhase(1)) {
-           if(option == 1) {
-               if (Teleports.canTeleport(player, true, TeleportType.GENERIC)) {
-                   Teleports.basicTeleport(player, new Tile(2990,3289));
-               }
-           }
-           if(option == 2) {
-               if (Teleports.canTeleport(player, true, TeleportType.GENERIC)) {
-                   Teleports.basicTeleport(player, new Tile(2984,3520));
-               }
-           }
-           if(option == 3) {
-               if (Teleports.canTeleport(player, true, TeleportType.GENERIC)) {
-                   Teleports.basicTeleport(player, new Tile(3175,3167));
-               }
-           }
-           if(option == 4) {
-               if (Teleports.canTeleport(player, true, TeleportType.GENERIC)) {
-                   Teleports.basicTeleport(player, new Tile(3301,3470));
-               }
-           }
-           if(option == 5) {
-               send(DialogueType.OPTION, DEFAULT_OPTION_TITLE, "Fire altar", "Body altar", "Cosmic altar", "Chaos altar ("+ Color.RED.wrap("Wilderness!")+")", "More...");
-               setPhase(2);
-           }
-       } else if(isPhase(2)) {
-           if(option == 1) {
-               if (Teleports.canTeleport(player, true, TeleportType.GENERIC)) {
-                   Teleports.basicTeleport(player, new Tile(3317,3251));
-               }
-           }
-           if(option == 2) {
-               if (Teleports.canTeleport(player, true, TeleportType.GENERIC)) {
-                   Teleports.basicTeleport(player, new Tile(3055,3441));
-               }
-           }
-           if(option == 3) {
-               if (Teleports.canTeleport(player, true, TeleportType.GENERIC)) {
-                   Teleports.basicTeleport(player, new Tile(2405,4381));
-               }
-           }
-           if(option == 4) {
-               if (Teleports.canTeleport(player, true, TeleportType.GENERIC)) {
-                   Teleports.basicTeleport(player, new Tile(3062,3588));
-               }
-           }
-           if(option == 5) {
-               send(DialogueType.OPTION, DEFAULT_OPTION_TITLE, "Astral altar", "Nature altar", "Law altar", "Death altar", "More...");
-               setPhase(3);
-           }
-       } else if(isPhase(3)) {
-           if(option == 1) {
-               if (Teleports.canTeleport(player, true, TeleportType.GENERIC)) {
-                   Teleports.basicTeleport(player, new Tile(2158,3857));
-               }
-           }
-           if(option == 2) {
-               if (Teleports.canTeleport(player, true, TeleportType.GENERIC)) {
-                   Teleports.basicTeleport(player, new Tile(2874,3018));
-               }
-           }
-           if(option == 3) {
-               if (Teleports.canTeleport(player, true, TeleportType.GENERIC)) {
-                   Teleports.basicTeleport(player, new Tile(2857,3377));
-               }
-           }
-           if(option == 4) {
-               if (Teleports.canTeleport(player, true, TeleportType.GENERIC)) {
-                   Teleports.basicTeleport(player, new Tile(1863,4635));
-               }
-           }
-           if(option == 5) {
-               send(DialogueType.OPTION, DEFAULT_OPTION_TITLE, "Blood altar", "Soul altar", "Wrath altar", "Nevermind");
-               setPhase(4);
-           }
-       } else if(isPhase(4)) {
            if(option == 1) {
                if (Teleports.canTeleport(player, true, TeleportType.GENERIC)) {
                    Teleports.basicTeleport(player, new Tile(1732,3828));
@@ -128,12 +47,7 @@ public class RunecraftingTeleportsD extends Dialogue {
                }
            }
            if(option == 3) {
-               if (Teleports.canTeleport(player, true, TeleportType.GENERIC)) {
-                   Teleports.basicTeleport(player, new Tile(2448,2822));
-               }
-           }
-           if(option == 4) {
-               stop();
+              stop();
            }
        }
     }
