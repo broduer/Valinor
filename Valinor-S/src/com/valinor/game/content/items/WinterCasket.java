15027f3924be10a2bd19f27bc884fb5f8ec26f43
Patrick van Elderen <patrick.vanelderen@live.nl>
2022-02-09 16:26:41 +0100

    no message

--- Valinor-S/src/com/valinor/game/content/items/WinterCasket.java
+++ Valinor-S/src/com/valinor/game/content/items/WinterCasket.java
@@ -9,8 +9,8 @@ import com.valinor.util.Utils;
 import java.util.Arrays;
 import java.util.List;
 
-import static com.valinor.util.CustomItemIdentifiers.WINTER_CASKET;
-import static com.valinor.util.ItemIdentifiers.HEFTY_TAX_BAG;
+import static com.valinor.util.CustomItemIdentifiers.*;
+import static com.valinor.util.ItemIdentifiers.*;
 
 /**
  * @author Patrick van Elderen <https://github.com/PVE95>
@@ -42,15 +42,40 @@ public class WinterCasket extends Interaction {
     }
 
     private final List<Item> COMMON_REWARDS = Arrays.asList(
-
+        new Item(LARRANS_KEY,10),
+        new Item(ABYSSAL_WHIP),
+        new Item(AMULET_OF_FURY),
+        new Item(BONECRUSHER),
+        new Item(EXPLORERS_RING_4),
+        new Item(BARROWS_MYSTERY_BOX)
     );
 
     private final List<Item> UNCOMMON_REWARDS = Arrays.asList(
-        new Item(HEFTY_TAX_BAG)
+        new Item(BANDOS_GODSWORD_ORNAMENT_KIT),
+        new Item(SARADOMIN_GODSWORD_ORNAMENT_KIT),
+        new Item(ZAMORAK_GODSWORD_ORNAMENT_KIT),
+        new Item(ARMADYL_GODSWORD_ORNAMENT_KIT),
+        new Item(TORMENTED_ORNAMENT_KIT),
+        new Item(WILDERNESS_KEY,3),
+        new Item(DONATOR_MYSTERY_BOX)
     );
 
     private final List<Item> RARE_REWARDS = Arrays.asList(
-
+        new Item(CRYSTAL_OF_ITHELL),
+        new Item(CRYSTAL_OF_IORWERTH),
+        new Item(CRYSTAL_OF_TRAHAEARN),
+        new Item(CRYSTAL_OF_CADARN),
+        new Item(CRYSTAL_OF_CRWYS),
+        new Item(CRYSTAL_OF_MEILYR),
+        new Item(CRYSTAL_OF_HEFIN),
+        new Item(CRYSTAL_OF_AMLODD),
+        new Item(PETS_MYSTERY_BOX),
+        new Item(CORRUPTING_STONE),
+        new Item(CURSED_AMULET_OF_THE_DAMNED),
+        new Item(FEROCIOUS_GLOVES),
+        new Item(SUPER_MYSTERY_BOX),
+        new Item(NIGHTMARE_STAFF),
+        new Item(RED_DYE)
     );
 
     private boolean rare = false;
